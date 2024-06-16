dba6847f8eebbe3c215d6676fb8b0d228d86021b
VaskorCb <vchakroborty096@gmail.com>
2024-06-17 00:31:01 +0600

    wdf

--- src/secret.js
+++ src/secret.js
@@ -1,19 +1,23 @@
-require('dotenv').config()
-const serverPort = process.env.SERVER_PORT || 4001
-const mongodbURL = process.env.MONGODB_ATLAS_URL || 'mongodb://localhost:27017/telecomDB'
-const defaultImagePath = process.env.DEFAULT_USER_IMAGE_PATH || 'public/images/users/rasel.jpg'
-const jwtActivationKey = process.env.JWT_ACTIVATION_KEY || 'hgffg_u88hbbb'
-const smtpUsername = process.env.SMTP_USERNAME || ''
-const smtpPassword = process.env.SMTP_PASSWORD || ''
-const clientURL = process.env.CLIENT_URL || ''
+require("dotenv").config();
+const serverPort = process.env.SERVER_PORT || 4001;
+const mongodbURL =
+  process.env.MONGODB_ATLAS_URL ||
+  "mongodb+srv://raseltechnext:rasrasras@cluster0.5emovqu.mongodb.net/telecomDB";
+const defaultImagePath =
+  process.env.DEFAULT_USER_IMAGE_PATH || "public/images/users/rasel.jpg";
+const jwtActivationKey = process.env.JWT_ACTIVATION_KEY || "hgffg_u88hbbb";
+const smtpUsername = process.env.SMTP_USERNAME || "";
+const smtpPassword = process.env.SMTP_PASSWORD || "";
+const clientURL = process.env.CLIENT_URL || "";
 
+console.log("mongodbURLOOO", mongodbURL);
 
 module.exports = {
-    serverPort,
-    mongodbURL,
-    defaultImagePath,
-    jwtActivationKey,
-    smtpUsername,
-    smtpPassword,
-    clientURL
-}
+  serverPort,
+  mongodbURL,
+  defaultImagePath,
+  jwtActivationKey,
+  smtpUsername,
+  smtpPassword,
+  clientURL,
+};
