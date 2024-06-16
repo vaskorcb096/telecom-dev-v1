9ba3fa53159c0a1dd12ad502c2227cbd29773250
VaskorCb <vchakroborty096@gmail.com>
2024-06-17 00:11:10 +0600

    update

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
-const app = require('./app')
-const connectDatabase = require('./config/db')
-const { serverPort } = require('./secret')
+const app = require("./app");
+const connectDatabase = require("./config/db");
+const { serverPort } = require("./secret");
 
 app.listen(serverPort, async () => {
-    console.log('Server is running')
-   await connectDatabase()
-})
+  console.log("Server is running", mongodbURL);
+  await connectDatabase();
+});
