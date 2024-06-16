0a088a784213226814882adcb357d2e70ae6daf2
VaskorCb <vchakroborty096@gmail.com>
2024-06-17 00:14:37 +0600

    kj

--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ const connectDatabase = require("./config/db");
 const { serverPort } = require("./secret");
 
 app.listen(serverPort, async () => {
-  console.log("Server is running", mongodbURL);
+  console.log("Server is running", mongodbURL, "Another one ");
   await connectDatabase();
 });
