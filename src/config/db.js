c3c1111941c4bd246700d6a5e87c62b44aaeff22
VaskorCb <vchakroborty096@gmail.com>
2024-06-17 00:18:57 +0600

    ftg

--- src/config/db.js
+++ src/config/db.js
@@ -1,17 +1,18 @@
-const mongoose = require('mongoose')
-const { mongodbURL } = require('../secret')
+const mongoose = require("mongoose");
+const { mongodbURL } = require("../secret");
 
 const connectDatabase = async (options = {}) => {
-    try {
-        await mongoose.connect(mongodbURL, options)
-        console.log("DB connected")
+  console.log(options, "is it right ", mongodbURL);
+  try {
+    await mongoose.connect(mongodbURL, options);
+    console.log("DB connected");
 
-        mongoose.connection.on('error', (error) => {
-            console.error("DB connection error: ", error)
-        })
-    } catch (error) {
-        console.error("Could not connect to DB: ", error.toString())
-    }
-}
+    mongoose.connection.on("error", (error) => {
+      console.error("DB connection error: ", error);
+    });
+  } catch (error) {
+    console.error("Could not connect to DB: ", error.toString());
+  }
+};
 
-module.exports = connectDatabase
+module.exports = connectDatabase;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,6 @@ const connectDatabase = require("./config/db");
 const { serverPort } = require("./secret");
 
 app.listen(serverPort, async () => {
-  console.log("Server is running", mongodbURL, "Another one ");
+  console.log("Server is running", serverPort, "Another one ");
   await connectDatabase();
 });
